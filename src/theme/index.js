8402f8c14f72eb1d2d73cf2d7e4e545744abb241
truong hoang nguyen <truonghoangnguyen@gmail.com>
2026-08-12 09:27:08 +0700

    docs: remove usage instructions for registerNextPrevNav from index.js

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,24 +1,6 @@
 import NextPrevNav from './NextPrevNav.vue';
 import { useNextPrevNav } from './useNextPrevNav.js';
 
-/**
- * registerNextPrevNav
- *
- * Cách dùng trong .vitepress/theme/index.js của user:
- *
- *   import DefaultTheme from 'vitepress/theme';
- *   import { registerNextPrevNav } from 'vitepress-next-prev-nav/theme';
- *
- *   export default {
- *     extends: DefaultTheme,
- *     enhanceApp({ app }) {
- *       registerNextPrevNav(app);
- *     }
- *   };
- *
- * Sau đó dùng trong layout / component khác:
- *   <NextPrevNav />
- */
 export function registerNextPrevNav(app) {
   app.component('NextPrevNav', NextPrevNav);
 }
